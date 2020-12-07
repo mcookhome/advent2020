4bb3d0c9de854fcfe097c07b53f5bb52cb0fea16
mcookhome <mcookhome@gmail.com>
2020-12-07 00:11:47 +0000

    day5

---
+++ day5/part1.py
@@ -0,0 +1,7 @@
+
+with open("input.txt", "r") as file:
+    entries = [entry.strip() for entry in file]
+    binary_seats = [(row[:7].replace("B", "1").replace("F", "0"), row[7:].replace("R", "1").replace("L", "0")) for row in entries]
+    seats = [(int(seat[0], 2), int(seat[1], 2)) for seat in binary_seats]
+    seat_ids = [8*seat[0]+seat[1] for seat in seats] 
+    print(max(seat_ids))
